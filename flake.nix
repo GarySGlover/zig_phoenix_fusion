a6ab58d0454ec19ee13bc8f76306a20a172482f5
GarySGlover <dev@thebigg69.co.uk>
2024-04-25 19:55:12 +0100

    Initial commit

---
+++ flake.nix
@@ -0,0 +1,24 @@
+{
+  description = "Flake for setting up Nim for PI Pico Development";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+    ...
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = import nixpkgs {
+        inherit system;
+      };
+    in {
+      devShells.default = pkgs.mkShell {
+        packages = with pkgs; [
+          zig_0_12
+          zls
+        ];
+      };
+    });
+}
